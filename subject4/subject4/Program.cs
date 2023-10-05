4abdc5a6fc1a8f9ed4b4b92b71d5eb361c387b74
Daffa Azka <destrcode@gmail.com>
2023-10-05 10:32:30 +0700

    0.4

---
+++ subject4/subject4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Subject4
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            Console.Write("Enter value item: ");
+            int value = int.Parse(Console.ReadLine());
+
+            if (value >= 10)
+            {
+                Console.WriteLine("Anda mendapatkan diskon 10%");
+            }
+            Console.WriteLine("Selesai");
+        }
+
+    }
+}
