af03cbf17be12466296b448644a45002410e116a
Daffa Azka <destrcode@gmail.com>
2023-11-02 10:13:07 +0700

    0.9

---
+++ subject7/subject7/Program.cs
@@ -0,0 +1,72 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Deklarasi Variabele
+        String? nama, keterangan, grade;
+        int nilaiTugas, nilaiUTS, nilaiSemester;
+        double hasil;
+
+        // Input Data nama
+        Console.Write("Masukan Nama Siswa : ");
+        nama = Console.ReadLine();
+
+        // Input Data nilaiTugas
+        nilaiTugas = InputInteger("tugas");
+
+
+        // Input Data nilaiUTS
+        nilaiUTS = InputInteger("UTS");
+
+        // Input Data nilaiSemester
+        nilaiSemester = InputInteger("Semester");
+
+        hasil = (nilaiTugas + nilaiUTS + nilaiSemester) / 3;
+        keterangan = (hasil >= 60) ? "Lulus" : "Silahlan mengambil remedial";
+
+        if (hasil >= 80)
+        {
+            grade = "A";
+        }
+        else if (hasil >= 70)
+        {
+            grade = "B";
+        }
+        else if (hasil >= 60)
+        {
+            grade = "C";
+        }
+        else if (hasil >= 50)
+        {
+            grade = "D";
+        }
+        else
+        {
+            grade = "E";
+        }
+
+        Console.WriteLine("Nilai Rapot : " + hasil);
+        Console.WriteLine("Keterangan  : " + keterangan);
+        Console.WriteLine("Grade       : " + grade);
+
+    }
+
+
+    // Function Validasi Data Yang Di Input Adalah Int Bukan String
+    public static int InputInteger(String s)
+    {
+        while (true)
+        {
+            try
+            {
+                Console.Write("Masukan nilai " + s + " : ");
+                int e = int.Parse(Console.ReadLine());
+                return e;
+            }
+            catch
+            {
+                Console.WriteLine("Input " + s + " yang anda masukan bukan angka, silahkan masukan ulang!");
+            }
+        }
+    }
+}
---
+++ subject8/subject8/Program.cs
@@ -0,0 +1,36 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        char kar;
+        Console.Write("Masukan buruk: ");
+        kar = Console.ReadLine()[0];
+
+        switch (kar)
+        {
+            case 'a':
+                Console.WriteLine("Huruf Vocal");
+                break;
+
+            case 'i':
+                Console.WriteLine("Huruf Vocal");
+                break;
+
+            case 'u':
+                Console.WriteLine("Huruf Vocal");
+                break;
+
+            case 'e':
+                Console.WriteLine("Huruf Vocal");
+                break;
+
+            case 'o':
+                Console.WriteLine("Huruf Vocal");
+                break;
+
+            default : 
+                Console.WriteLine("Huruf Konsonan");
+                break;
+        }
+    }
+}
