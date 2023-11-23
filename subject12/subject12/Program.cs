950ccc96f75b6f325021c110befb50b0105abc45
Daffa Azka <destrcode@gmail.com>
2023-11-23 09:03:41 +0700

    0.14

--- subject12/subject12/Program.cs
+++ subject12/subject12/Program.cs
@@ -4,7 +4,7 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        task3();
+        task5();
     }
 
     private static void  task1()
@@ -23,5 +23,22 @@ internal class Program
     {
         int i = 40;
         do { Console.WriteLine(i); i-=2; } while (i >= 20);
+    }
+
+    private static void task4()
+    {
+        int i = 10;
+        int j = 70;
+        do { Console.WriteLine(i + " " + j); i+=10; j -= 10; } while (i <= 50);
+    }
+
+    private static void task5()
+    {
+        int i = 1;
+        int j = 0;
+        do { 
+            Console.WriteLine(i);
+            i += i;
+        } while (i <= 22);
     }
 }
---
+++ subject13/subject13/Program.cs
@@ -0,0 +1,37 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Task2();
+    }
+
+    private static void Example()
+    {
+        for (int i = 0; i < 6; i++)
+        {
+            Console.WriteLine($"Loop-{i + 1}");
+        }
+        Console.WriteLine("Thanks");
+    }
+
+    private static void Task1() {
+
+        for (int i = 50; i >= 40; i--)
+        {
+            Console.WriteLine($"Loop-{i}");
+        }
+        Console.WriteLine("Thanks");
+
+    }
+
+    private static void Task2()
+    {
+
+        for (int i = 31; i <= 40; i +=2)
+        {
+            Console.WriteLine($"Loop-{i}");
+        }
+        Console.WriteLine("Thanks");
+
+    }
+}
