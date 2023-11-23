11f25ceeb18b4b19cb951eb60b5de5957312ee1a
Daffa Azka <destrcode@gmail.com>
2023-11-23 10:57:58 +0700

    0.15

---
+++ subject15/subject15/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var i = 5;
+        var j = 30;
+        var k = 1;
+
+        while(i <= 26)
+        {
+            Console.WriteLine($"{i} {j}");
+            i += k;
+            j -= k;
+            k++;
+        }
+    }
+}
