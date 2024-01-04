995429fcf723c7ba1eec772bed74d050acdb056f
Daffa Azka <destrcode@gmail.com>
2024-01-04 07:56:14 +0700

    1.1

---
+++ S2/subject1/subject1/Utilities.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace subject1
+{
+    public static class Utilities
+    {
+        public static int input(String s)
+        {
+            Console.Write($"{s}: ");
+            return getOutput();
+        }
+
+        private static int getOutput() {
+            int n = 0;
+            while (true)
+            {
+                try
+                {
+                    n = int.Parse(Console.ReadLine());
+                    break;
+                } catch ( Exception e )
+                {
+                    Console.Write($"Error Exception (Silahkan Input Ulang) : ");
+                }
+            }
+            return n;
+        }
+
+    }
+}
